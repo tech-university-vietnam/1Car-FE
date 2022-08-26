75903dd4b93e805685c823fffeee8ffda63596ff
Nina Huynh <Nina_Huynh@mckinsey.com>
2022-08-26 08:54:27 +0700

    Change to allow calling without user consent

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import AppRouters from './Routes';
 import ErrorBoundary from './components/ErrorBoundary';
 import Cookies from "universal-cookie";
@@ -9,9 +9,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 function App() {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
-  
+
   useEffect(() => {
-    const getAccessToken = async () => { 
+    const getAccessToken = async () => {
       try {
         if (isAuthenticated) {
           const accessToken = await getAccessTokenSilently({
@@ -21,15 +21,13 @@ function App() {
           const cookies = new Cookies();
           cookies.set('access_token', accessToken, { path: "/" });
         }
-        
+
       } catch (e: any) {
         console.log(e.message)
       }
     }
     getAccessToken();
-  }, [getAccessTokenSilently]);
-
-  useEffect(() => console.log(isAuthenticated), [isAuthenticated]);
+  }, [getAccessTokenSilently, domain, isAuthenticated]);
 
   return (
     <ErrorBoundary>
--- src/components/auth/AuthButtons.tsx
+++ src/components/auth/AuthButtons.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import AntButton from "../AntButton";
 import Cookies from "universal-cookie";
 
 export const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <AntButton onClickFunction={loginWithRedirect} label="Sign in"/>
+  return <AntButton onClickFunction={() => loginWithRedirect(
+    { "prompt": "consent", "scope": "read:current_user" }
+  )} label="Sign in" />
 }
 
 export const LogoutButton = () => {
   const { logout } = useAuth0();
 
-  return <AntButton onClickFunction={() => logout({returnTo: window.location.origin})}
-                    label="Logout"
+  return <AntButton onClickFunction={() => logout({ returnTo: window.location.origin })}
+    label="Logout"
   />
 }
 
 export const UserProfileButton = () => {
-  const {user, isAuthenticated, getAccessTokenSilently} = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   const [userMetaData, setUserMetaData] = useState(null);
 
   const cookies = new Cookies();
@@ -28,7 +30,7 @@ export const UserProfileButton = () => {
 
       try {
         const accessToken = cookies.get('access_token');
-        if (accessToken) { 
+        if (accessToken) {
           const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
           const metadataResponse = await fetch(userDetailsByIdUrl, {
             headers: {
@@ -44,10 +46,10 @@ export const UserProfileButton = () => {
       }
     }
     getUserMetaData();
-  }, [getAccessTokenSilently, user?.sub])
+  }, [user?.sub])
 
   return (
-    isAuthenticated && (
+    (
       <div>
         <p>Hi, {user?.name}</p>
       </div>

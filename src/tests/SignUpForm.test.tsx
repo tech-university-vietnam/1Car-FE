90099fe369f940e74c37d155905dd3ae871d7f24
nekomi2 <dore123222@gmail.com>
2022-08-27 22:49:54 +0700

    ran eslint --fix

--- src/tests/SignUpForm.test.tsx
+++ src/tests/SignUpForm.test.tsx
@@ -1,11 +1,11 @@
-import React, { useRef } from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { SignUpForm } from "../components/SignUpModal";
-import userEvent from "@testing-library/user-event";
+import React, { useRef } from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { SignUpForm } from '../components/SignUpModal';
+import userEvent from '@testing-library/user-event';
 
-describe("SignUpForm component", () => {
+describe('SignUpForm component', () => {
   beforeEach(() => {
-    Object.defineProperty(window, "matchMedia", {
+    Object.defineProperty(window, 'matchMedia', {
       writable: true,
       value: jest.fn().mockImplementation((query) => ({
         matches: false,
@@ -20,7 +20,7 @@ describe("SignUpForm component", () => {
     });
   });
 
-  test("It should render initial form", async () => {
+  test('It should render initial form', async () => {
     render(<SignUpForm />);
 
     const inputName = screen.getByPlaceholderText(/name/i);
@@ -28,28 +28,28 @@ describe("SignUpForm component", () => {
     const inputPassword = screen.getByPlaceholderText(/password/i);
     const inputDateOfBirth = screen.getByPlaceholderText(/Date of birth/i);
     const checkboxAccept = screen.getByLabelText(/accept/i);
-    const signUpButton = screen.getByRole("button", { name: "Sign Up" });
+    const signUpButton = screen.getByRole('button', { name: 'Sign Up' });
 
-    expect(inputName).toHaveValue("");
-    expect(inputEmail).toHaveValue("");
-    expect(inputPassword).toHaveValue("");
-    expect(inputDateOfBirth).toHaveValue("");
+    expect(inputName).toHaveValue('');
+    expect(inputEmail).toHaveValue('');
+    expect(inputPassword).toHaveValue('');
+    expect(inputDateOfBirth).toHaveValue('');
     expect(checkboxAccept).not.toBeChecked();
     expect(signUpButton).toBeDisabled();
   });
 
-  test("It should prompt error label when input wrong value", async () => {
+  test('It should prompt error label when input wrong value', async () => {
     render(<SignUpForm />);
 
     const inputEmail = screen.getByPlaceholderText(/email/i);
 
-    userEvent.type(inputEmail, "test");
+    userEvent.type(inputEmail, 'test');
     userEvent.tab();
     const errorEmailLabel = await screen.findByText(/please enter your email/i);
     expect(errorEmailLabel).toBeInTheDocument();
   });
 
-  test("It should enable login button when all inputs are filled", async () => {
+  test('It should enable login button when all inputs are filled', async () => {
     render(<SignUpForm />);
 
     const inputName = screen.getByPlaceholderText(/name/i);
@@ -57,13 +57,13 @@ describe("SignUpForm component", () => {
     const inputPassword = screen.getByPlaceholderText(/password/i);
     const inputDateOfBirth = screen.getByPlaceholderText(/Date of birth/i);
     const checkboxAccept = screen.getByLabelText(/accept/i);
-    const signUpButton = screen.getByRole("button", { name: "Sign Up" });
+    const signUpButton = screen.getByRole('button', { name: 'Sign Up' });
 
-    userEvent.type(inputName, "Test name");
-    userEvent.type(inputEmail, "testemail@gmail.com");
-    userEvent.type(inputDateOfBirth, "2001-02-24");
-    userEvent.keyboard("{enter}");
-    userEvent.type(inputPassword, "fake password");
+    userEvent.type(inputName, 'Test name');
+    userEvent.type(inputEmail, 'testemail@gmail.com');
+    userEvent.type(inputDateOfBirth, '2001-02-24');
+    userEvent.keyboard('{enter}');
+    userEvent.type(inputPassword, 'fake password');
     userEvent.click(checkboxAccept);
 
     await waitFor(() => {
@@ -71,7 +71,7 @@ describe("SignUpForm component", () => {
     });
   });
 
-  test("It should disable login button when submit the form", async () => {
+  test('It should disable login button when submit the form', async () => {
     render(<SignUpForm />);
     jest.useFakeTimers();
 
@@ -80,13 +80,13 @@ describe("SignUpForm component", () => {
     const inputPassword = screen.getByPlaceholderText(/password/i);
     const inputDateOfBirth = screen.getByPlaceholderText(/Date of birth/i);
     const checkboxAccept = screen.getByLabelText(/accept/i);
-    const signUpButton = screen.getByRole("button", { name: "Sign Up" });
+    const signUpButton = screen.getByRole('button', { name: 'Sign Up' });
 
-    userEvent.type(inputName, "Test name");
-    userEvent.type(inputEmail, "testemail@gmail.com");
-    userEvent.type(inputDateOfBirth, "2001-02-24");
-    userEvent.keyboard("{enter}");
-    userEvent.type(inputPassword, "fake password");
+    userEvent.type(inputName, 'Test name');
+    userEvent.type(inputEmail, 'testemail@gmail.com');
+    userEvent.type(inputDateOfBirth, '2001-02-24');
+    userEvent.keyboard('{enter}');
+    userEvent.type(inputPassword, 'fake password');
     userEvent.click(checkboxAccept);
     userEvent.click(signUpButton);
 

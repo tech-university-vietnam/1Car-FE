d6d9ee6e21ff75bae8430511e91a9938a999ccb0
Vu Duong <53283429+congvu24@users.noreply.github.com>
2022-09-06 16:38:59 +0700

    feat: redux setup and homepage integertion with backend (#8)
    Co-authored-by: Vu Duong <vu_duong_cong@mckinsey.vn>

--- src/pages/Homepage/SelectFilter.tsx
+++ src/pages/Homepage/SelectFilter.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Select } from 'antd';
+import { Attribute, Type } from '../../redux/reducer/car';
 
-export default function SelectFilter() {
+export default function SelectFilter({
+  type,
+  data,
+  currentFilterAttribute,
+  updateFilter,
+}: {
+  type: Type;
+  data: Attribute[];
+  currentFilterAttribute: string[];
+  updateFilter: (data: string) => void;
+}) {
+  const onChange = (value: string) => {
+    updateFilter(value);
+  };
+
+  const currentId = currentFilterAttribute.find((item) =>
+    data.map((item) => item.id).includes(item)
+  );
+
   return (
-    <div className="mr-2 mb-4 w-full basis-1/4 md:mr-4 md:mb-0 md:w-36 md:basis-1/6">
-      <Select placeholder="Brand" className="w-full" size="large">
-        <Select.Option value="Audi">Audi</Select.Option>
-        <Select.Option value="Toyota">Toyota</Select.Option>
-        <Select.Option value="Audi">Ferrari</Select.Option>
+    <div className='mr-2 mb-4 w-full basis-1/4 md:mr-4 md:mb-0 md:w-36 md:basis-1/6'>
+      <Select
+        placeholder={type.type}
+        className='w-full'
+        size='large'
+        onChange={onChange}
+        value={currentId}
+      >
+        {data.map((item) => (
+          <Select.Option key={item.id} value={item.id}>
+            {item.value}
+          </Select.Option>
+        ))}
       </Select>
     </div>
   );
